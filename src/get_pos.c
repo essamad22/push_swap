c4101aa889ec042ff94754571b55e24ebd96bb50
Abdessamad Akhtab <aakhtab@e2r1p10.1337.ma>
2023-06-24 00:12:03 +0100

    add

--- src/get_pos.c
+++ src/get_pos.c
@@ -6,12 +6,18 @@
 /*   By: aakhtab <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 19:39:35 by aakhtab           #+#    #+#             */
-/*   Updated: 2023/06/23 22:24:32 by aakhtab          ###   ########.fr       */
+/*   Updated: 2023/06/24 00:11:30 by aakhtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	first_half(int a_pos, int b_pos)
+{
+	if (a >= 0 && b >= 0)
+	{
+		
+}
 int	get_b_pos(int len_b, int index)
 {
 	if (index <= (len_b / 2))
@@ -23,7 +29,7 @@ int	get_b_pos(int len_b, int index)
 void	moves_count(t_pos **pos, int index, int i)
 {
 	int	b_pos;
-	//int moves;
+	int moves;
 
 	if (index == 0)
 	{
@@ -33,4 +39,5 @@ void	moves_count(t_pos **pos, int index, int i)
 		return ;
 	}
 	b_pos = get_b_pos((**pos).len_b, index);
+	moves = first_half(i, index);
 }
